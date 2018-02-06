0dc764113aad930111b5961739e523ce68c87bb2
sstone18 <shawnrstone1995@gmail.com>
2018-02-06 17:01:09 -0500

    updated links and added contact

--- resources/js/index.js
+++ resources/js/index.js
@@ -39,17 +39,48 @@ $(function() {
 	}, 200);*/
 
 
+
+
+	// Add smooth scrolling to all links
+	$("a").on('click', function(event) {
+
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
+
+			// Store hash
+			var hash = this.hash;
+
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function() {
+
+				// Add hash (#) to URL when done scrolling (default click behavior)
+				window.location.hash = hash;
+			});
+		} // End if
+	});
+
+
+
 })
 
 
 /* Set the width of the side navigation to 250px */
-	function openNav() {
-	    document.getElementById("sideNav").style.width = "12rem";
-			document.getElementById("main").style.width = "auto";
-	}
+function openNav() {
+	document.getElementById("sideNav").style.width = "12rem";
+	document.getElementById("main").style.width = "auto";
+}
 
-	/* Set the width of the side navigation to 0 */
-	function closeNav() {
-	    document.getElementById("sideNav").style.width = "0";
-			document.getElementById("main").style.width = "0";
-	}
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+	document.getElementById("sideNav").style.width = "0";
+	document.getElementById("main").style.width = "0";
+}
+
+function workLink() {
+	
+}
