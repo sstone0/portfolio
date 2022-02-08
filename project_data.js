22465f0b103bbfac2664a8a3b8ea33f3195e0b83
sstone0 <shawnrstone1995@gmail.com>
2022-02-07 22:08:38 -0500

    updated project data

--- project_data.js
+++ project_data.js
@@ -4,7 +4,16 @@ const PROJECT_DATA = [
 		title: 'CRWN-Clothing',
 		description: 'This is the sample description about the store.',
 		image: '/crwn-clothing2.png',
-		technology: ['React', 'Redux', 'Thunk', 'Firebase'],
+		technology: [
+			'React',
+			'Redux',
+			'Redux Sagas',
+			'React Hooks',
+			'GraphQL',
+			'ContextAPI',
+			'Stripe',
+			'Firebase',
+		],
 		links: {
 			github_url: 'https://github.com/sstone0/crwn-clothing_redo',
 			url: 'https://sstone0.github.io/crwn-clothing_redo/',
@@ -16,7 +25,13 @@ const PROJECT_DATA = [
 		description:
 			'This is the sample description about the Yelp Search project.',
 		image: '/yelp-search2.png',
-		technology: ['React', 'React Hooks', 'React Context', 'Yelp API'],
+		technology: [
+			'Nextjs',
+			'React',
+			'React Hooks',
+			'React ContextAPI',
+			'Yelp API',
+		],
 		links: {
 			github_url: 'https://github.com/sstone0/yelp-search',
 			url: 'https://yelp-search-app.vercel.app/',
